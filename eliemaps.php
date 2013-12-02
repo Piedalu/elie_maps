47eb42887547d9ca21f4c7077389a07ee774d729
Élie Deilhes <elie.deilhes.1@ulaval.ca>
2013-12-02 15:03:39 -0500

    Paramétrage du panneau d'administration
    Ajout de labels
    Ajout d'un champ personnalisé pour saisir l'adresse

--- eliemaps.php
+++ eliemaps.php
@@ -12,14 +12,47 @@
 /*
 Prévoir un panneau d'administration pour générer la carte à partir d'une adresse
 Possibilité d'enregistrer plusieurs cartes.
+
 */
 
 /* Initialisation */
 add_action('init', 'eliemaps_init');
+add_action('add_meta_boxes', 'eliemaps_metabox');
 
 /* Permet d'initialiser le panneau d'administration des cartes */
 function eliemaps_init(){
-	register_post_type('carte', array('public' => TRUE, 'label' => 'Cartes'));
+
+	$labels = array(
+		'name' => 'Cartes',
+		'singular_name' => 'carte',
+		'menu_name' => 'Eliemaps',
+		'add_new' => 'Ajouter une carte',
+		'add_new_item' => 'Ajouter une nouvelle carte',
+		'edit_item' => 'Editer une carte',
+		'new_item' => 'Nouvelle carte',
+		'view_item' => 'Voir la carte',
+		'search_item' => 'Rechercher une carte',
+		'not_found' => 'Aucune carte trouvée',
+		'not_found_in_trash' => 'Aucune carte trouvée dans la corbeille',
+		);
+
+	register_post_type('eliemaps', array(
+		'public' => TRUE,
+		'publicly_queryable' => FALSE,
+		'labels' => $labels,
+		'supports' => array('title')
+		));
+}
+
+function eliemaps_metabox(){
+	add_meta_box('adresse', 'Adresse', 'eliemaps_metabox_adresse', 'eliemaps');
+}
+
+function eliemaps_metabox_adresse($id){
+	?>
+	<label>Adresse:</label>
+	<input name="adresse" value="<?php echo $adresse; ?>" style="width:100%"/>
+	<?php
 }
 
 /* Affichage de la carte */
